6b85791da4e5d974c928bc8bde32fb410774b517
Jaideep Singh <jaisidhu2004@gmail.com>
2025-10-24 19:21:34 +0530

    24 oct 2025

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://video-backend.cloud"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
